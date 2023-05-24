42c5859753589ade2b56f8cc07d732d1c307896f
xoloshef <xoloshef@gmail.com>
2023-05-24 11:22:35 +0300

    zho 8.1 edit admin

--- ZavHozOffice/hozoffice/admin.py
+++ ZavHozOffice/hozoffice/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 
-from .models import Question, Workers, Assets, BaseActs
+from .models import Question, Choice, Workers, Assets, BaseActs
 
 # принимет до трех вот все модели: Question, Choice, Workers, Assets, BaseActs
 
-admin.site.register(Question)
+admin.site.register(BaseActs)
 admin.site.register(Workers)
 admin.site.register(Assets)
-admin.site.register(BaseActs)
+admin.site.register(Question)
+admin.site.register(Choice)
 
 # Register your models here.
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+admin.site.register(QuestionAdmin)
